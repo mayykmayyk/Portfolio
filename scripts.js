a2e7d79e2a0fbd23cb1f123520097830bd6875d2
Mike Rivera <mikoriv@gmail.com>
2021-09-09 12:32:33 +0800

    added JS animations

--- scripts.js
+++ scripts.js
@@ -1,20 +1,41 @@
-header = document.getElementById("mainHeader");
-listItem = document.querySelectorAll("li a");
-console.log(listItem);
-
-var changeHeaderBG = function(){
-    var y = window.scrollY;
-    if (y >= 50){
-        header.classList.add("add-background");
-        for (i in listItem){
-            listItem[i].classList.add("change-color");
-        }
-    } else{
-        header.classList.remove("add-background");
-        for (i in listItem){
-            listItem[i].classList.remove("change-color");
-        }
-    }
+const fade = document.querySelectorAll('.fade-in');
+const options = { 
+    threshold: .13,
+    rootMargin: "0px 0px -400px 0px"
 };
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, 
+options);
 
-window.addEventListener("scroll", changeHeaderBG);
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".hero");
+const sectionOneOptions = {
+    rootMargin: "-300px 0px 0px 0px"
+ };
+const sectionOneObserver = new IntersectionObserver(function(entries, sectionOneObserver) {
+    entries.forEach(entry =>{
+        if(!entry.isIntersecting){
+            header.classList.add('appear');
+        } else {
+            header.classList.remove('appear');
+        }
+    })
+}, sectionOneOptions);
+
+
+
+fade.forEach(fade =>{
+    appearOnScroll.observe(fade);
+})
+sectionOneObserver.observe(sectionOne);
+
+
+
